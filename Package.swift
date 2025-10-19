758e41c4cdb3f36c474c3560e2bd7f478992f9b1
batuhanbaran <fb_batuhan_baran@hotmail.com>
2025-10-19 21:57:57 +0300

    update navigator

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["TodoX"]
         ),
     ],
-    dependencies: [.package(url: "https://github.com/hmlongco/Navigator", exact: "1.0.0")],
+    dependencies: [.package(url: "https://github.com/hmlongco/Navigator.git", from: "1.3.1")],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
